8f1dbdbb7f03fb944eb80502ffe9ad82da3b7bc3
Ali Farrell <alastairfarrell@gmail.com>
2019-05-30 19:37:27 +0100

    Iains url

--- app/server.py
+++ app/server.py
@@ -10,10 +10,17 @@ from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
 #export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_url = 'https://drive.google.com/uc?export=download&id=1i6NUj9xOq4Te7yS-Ha9hhvZwO1yhKuCg'
+#export_file_url = 'https://drive.google.com/uc?export=download&id=1i6NUj9xOq4Te7yS-Ha9hhvZwO1yhKuCg'
+#export_file_name = 'export.pkl'
+
+#classes = ['rucksack', 'handbag']
+
+export_file_url = 'https://drive.google.com/uc?export=download&id=1Iy8gb9PKwYQ22BHVoX1XoQxbh9YRKgQM'
+
 export_file_name = 'export.pkl'
 
 classes = ['rucksack', 'handbag']
+
 path = Path(__file__).parent
 
 app = Starlette()
