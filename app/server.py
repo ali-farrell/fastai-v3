485246c716463d8316ebd628d157ccc295f5e9e8
Ali Farrell <alastairfarrell@gmail.com>
2019-05-30 19:12:23 +0100

    Added classes

--- app/server.py
+++ app/server.py
@@ -13,7 +13,7 @@ from starlette.staticfiles import StaticFiles
 export_file_url = 'https://drive.google.com/uc?export=download&id=1i6NUj9xOq4Te7yS-Ha9hhvZwO1yhKuCg'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['rucksack', 'handbag']
 path = Path(__file__).parent
 
 app = Starlette()
